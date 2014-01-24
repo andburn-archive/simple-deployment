0e92e59ca5e477d240e2254bdb3f94954353f524
Andrew Burnett <andrew@nospam.forme>
2014-01-24 14:50:18 +0000

    Revert to SQL block input

--- deploy_scripts/deploy.sh
+++ deploy_scripts/deploy.sh
@@ -130,7 +130,27 @@ console_message "Testing application"
 mv webpackage_preTest.tgz test
 cd test
 tar -zxvf webpackage_preTest.tgz
-testCgiScript
+
+# Start services
+/etc/init.d/apache2 start
+/etc/init.d/mysql start
+
+# create database
+cat <<FINISH | mysql -uroot -ppassword
+drop database if exists dbtest;
+CREATE DATABASE dbtest;
+GRANT ALL PRIVILEGES ON dbtest.* TO dbtestuser@localhost IDENTIFIED BY 'dbpassword';
+use dbtest;
+drop table if exists custdetails;
+create table if not exists custdetails (
+name         VARCHAR(30)   NOT NULL DEFAULT '',
+address         VARCHAR(30)   NOT NULL DEFAULT ''
+);
+insert into custdetails (name,address) values ('John Smith','Street Address'); select * from custdetails;
+FINISH
+
+perl -w apache/cgi-bin/accept_form.pl name="Bill Jones" address="No fixed abode"
+
 tar -zcvf ../webpackage_preDeploy.tgz apache
 
 # back up to sandbox level
