6e09f55ca197c895c3a9e182f49bfac747257746
Andrew Burnett <andrew@nospam.forme>
2014-01-24 10:35:34 +0000

    Add config file and start script

--- vbox_scripts/deploy.sh
+++ vbox_scripts/deploy.sh
@@ -1,30 +1,11 @@
 #!/usr/bin/bash
 
+# import config file
+source deploy.cfg
 # import some external functions
 source deploy_lib_helper.sh
 source deploy_lib_build.sh
 source deploy_lib_monitor.sh
 
-## RUNS ON BUILD SERVER ##
-
-REPO_NAME="simple_deployment"
-REPO_URL="https://github.com/andburn/$REPO_NAME.git"
-
-# updating apt repositories
-console_message "Updating apt repositories"
-apt-get -qq update
-echo 'apt-get update (done)'
-
-# check if git is installed
-git --version >/dev/null
-if [ $? -ne 0 ] ; then
-	console_message "Installing git"
-	apt-get install git
-fi
-
-# get deployment scripts
-console_message "Cloning source from GitHub"
-cd ~
-git clone $REPO_URL
-cd "$REPO_NAME/vbox_scripts"
+console_message "transfer complete!"
 ls -la
