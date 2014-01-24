2c68b6f1efae6717d1c12f307e788089c50062ac
Andrew Burnett <andrew@nospam.forme>
2014-01-24 16:42:10 +0000

    Add version option to start script

--- start_from_linux.sh
+++ start_from_linux.sh
@@ -1,2 +1,6 @@
+APP_VERSION=1
+if [ -n "$1" ] ; then
+	APP_VERSION=$1
+fi
 scp -P 2222 setup_env.sh testuser@127.0.0.1:~
-ssh -t -p 2222 testuser@127.0.0.1 'sudo bash ~/setup_env.sh'
+ssh -t -p 2222 testuser@127.0.0.1 "sudo bash ~/setup_env.sh $APP_VERSION"
