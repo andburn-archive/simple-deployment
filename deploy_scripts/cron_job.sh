887eee05e22ad231daf9b2717be016c8d2bf4e86
Andrew Burnett <andrew.burnett@student.ncirl.ie>
2014-01-25 14:05:49 +0000

    Fix cron monitor

--- deploy_scripts/cron_job.sh
+++ deploy_scripts/cron_job.sh
@@ -6,27 +6,37 @@ MONITOR_LOG="/dep_monitor/monitor.log"
 AWS_IP="54.194.174.13"
 
 function monitor_log {
-	# TODO: format better
+	# TODO: format tstamp
 	local tstamp=$(date)
 	echo "[$tstamp] $1" >> $MONITOR_LOG
 }
 
 function isAWSApacheUp {
-	isTCPremoteOpen $AWS_IP 80
+	nc -z -w 3 $AWS_IP 80
 	return $?
 }
 
 function isAWSMySqlUp {
-	isTCPremoteOpen $AWS_IP 3306
+	nc -z -w 3 $AWS_IP 3306
+	return $?
+}
+
+function isAppRunning {
+	curl "http://$AWS_IP:80" | grep "<title>Sample App</title>"
 	return $?
 }
 
 isAWSApacheUp
-if [ "$?" -eq 0 ]; then
-	monitor_log "AWS Apache is down"
+if [ "$?" -ne 0 ]; then
+	monitor_log "AWS Apache is down."
 fi
 
 isAWSMySqlUp
-if [ "$?" -eq 0 ]; then
-	monitor_log "AWS Mysql is down"
+if [ "$?" -ne 0 ]; then
+	monitor_log "AWS Mysql is down."
+fi
+
+isAppRunning
+if [ "$?" -ne 0 ]; then
+	monitor_log "Application is not running as expected."
 fi
